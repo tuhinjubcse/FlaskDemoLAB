692d2ff8af387dbbea53a716caf40b32485f8ed5
Aadit Trived <aadit.trivedi03@gmail.com>
2021-06-12 21:56:11 -0700

    merged apps

--- demo/metaphors/app.py
+++ demo/metaphors/app.py
@@ -7,12 +7,18 @@ from FnFrames import FnFrames
 
 app = Flask(__name__)
 counter = 0
+global new_sent
+new_sent=""
+first_el=""
+second_el=""
+third_el=""
 @app.route("/", methods=["GET", "POST"])
 def show_domains():
     global counter
 
     fn = FnFrames()
     options = []
+    
     if request.method == 'POST':
         if counter == 0:
             global input
@@ -21,15 +27,27 @@ def show_domains():
             possible_targets = possible_targets_frames_output[0]
             focus_word_index = possible_targets_frames_output[1]
             print("INDEX: " + str(focus_word_index))
+
+            # ONLY WORKS IS FOCUS WORD IS IN MIDDLE OF SENTENCE
+
             focus_word = input.split()[focus_word_index]
+            sent_split_focus = input.split(focus_word)
+            print(sent_split_focus)
+            global first_el
+            first_el = sent_split_focus[0]
+            global second_el
+            second_el = focus_word
+            global third_el
+            third_el = sent_split_focus[1]
             new_focus_word = "<b>" + focus_word + "</b>"
             new_sent = input.replace(focus_word,new_focus_word)
+            
             print("NEW SENT: " + str(new_sent))
             print(list(possible_targets.keys()))
             options = possible_targets[list(possible_targets.keys())[0]]
             counter = counter + 1
             return render_template(
-                "index.html", 
+                "index_final_tokens.html", 
                 options=options, 
                 input=input, 
                 show_domains="display: none;",
@@ -40,20 +58,27 @@ def show_domains():
                 source_domain_show="display: none;",
                 possible_sources=[],
                 dropdown_showtime="",
+                input_show="display: none;",
+                new_sent = new_sent,
+                first_el = first_el,
+                second_el = second_el,
+                third_el = third_el,
             )
         if counter == 1:
             print("HI")
             try:
+                global target_domain
                 target_domain = request.form["modelselector"]
+                global possible_sources
                 possible_sources = fn.get_possible_source_frames(target_domain)
 
-                print(target_domain)
+                counter = counter + 1
                 return render_template(
-                    "index.html", 
+                    "index_final_tokens.html", 
                     options=options, 
                     input=input, 
                     show_domains="display: none;",
-                    dropdown_show="",
+                    dropdown_show="display: none;",
                     continue_show="display: none;",
                     gen_button="",
                     target_selection_show = "",
@@ -61,17 +86,31 @@ def show_domains():
                     source_domain_show="",
                     possible_sources=possible_sources,
                     dropdown_showtime="display: none;",
+                    input_show="display: none;",
+                    new_sent = "",
+                    first_el = first_el,
+                    second_el = second_el,
+                    third_el = third_el,
 
                 )
             except Exception:
                 counter = 0
                 print("oops")
 
+        if counter == 2:
+            print("INPUT: " + str(input))
+            print("TARGET domain: " + str(target_domain))
+            source_domain = possible_sources[int(request.form["options"])]
+            print("SOURCE domain: " + str(source_domain))
+            counter = 0
 
-    
-    # if button is pressed
+            # RUN MODEL HERE
+            
+
+
+    # default homepage view
     return render_template(
-        "index.html", 
+        "index_final_tokens.html", 
         options=options,
         show_domains="", 
         dropdown_show="display: none;",
@@ -80,6 +119,11 @@ def show_domains():
         target_selection_show = "display: none;",
         source_domain_show="display: none;",
         possible_sources=[],
+        input_show="",
+        new_sent = "",
+        first_el = "",
+        second_el = "",
+        third_el = "",
     )
 
 
--- demo/metaphors/old/app.py
+++ demo/metaphors/old/app.py
@@ -7,18 +7,12 @@ from FnFrames import FnFrames
 
 app = Flask(__name__)
 counter = 0
-global new_sent
-new_sent=""
-first_el=""
-second_el=""
-third_el=""
 @app.route("/", methods=["GET", "POST"])
 def show_domains():
     global counter
 
     fn = FnFrames()
     options = []
-    
     if request.method == 'POST':
         if counter == 0:
             global input
@@ -27,27 +21,15 @@ def show_domains():
             possible_targets = possible_targets_frames_output[0]
             focus_word_index = possible_targets_frames_output[1]
             print("INDEX: " + str(focus_word_index))
-
-            # ONLY WORKS IS FOCUS WORD IS IN MIDDLE OF SENTENCE
-
             focus_word = input.split()[focus_word_index]
-            sent_split_focus = input.split(focus_word)
-            print(sent_split_focus)
-            global first_el
-            first_el = sent_split_focus[0]
-            global second_el
-            second_el = focus_word
-            global third_el
-            third_el = sent_split_focus[1]
             new_focus_word = "<b>" + focus_word + "</b>"
             new_sent = input.replace(focus_word,new_focus_word)
-            
             print("NEW SENT: " + str(new_sent))
             print(list(possible_targets.keys()))
             options = possible_targets[list(possible_targets.keys())[0]]
             counter = counter + 1
             return render_template(
-                "index_final_tokens.html", 
+                "index.html", 
                 options=options, 
                 input=input, 
                 show_domains="display: none;",
@@ -58,27 +40,20 @@ def show_domains():
                 source_domain_show="display: none;",
                 possible_sources=[],
                 dropdown_showtime="",
-                input_show="display: none;",
-                new_sent = new_sent,
-                first_el = first_el,
-                second_el = second_el,
-                third_el = third_el,
             )
         if counter == 1:
             print("HI")
             try:
-                global target_domain
                 target_domain = request.form["modelselector"]
-                global possible_sources
                 possible_sources = fn.get_possible_source_frames(target_domain)
 
-                counter = counter + 1
+                print(target_domain)
                 return render_template(
-                    "index_final_tokens.html", 
+                    "index.html", 
                     options=options, 
                     input=input, 
                     show_domains="display: none;",
-                    dropdown_show="display: none;",
+                    dropdown_show="",
                     continue_show="display: none;",
                     gen_button="",
                     target_selection_show = "",
@@ -86,31 +61,17 @@ def show_domains():
                     source_domain_show="",
                     possible_sources=possible_sources,
                     dropdown_showtime="display: none;",
-                    input_show="display: none;",
-                    new_sent = "",
-                    first_el = first_el,
-                    second_el = second_el,
-                    third_el = third_el,
 
                 )
             except Exception:
                 counter = 0
                 print("oops")
 
-        if counter == 2:
-            print("INPUT: " + str(input))
-            print("TARGET domain: " + str(target_domain))
-            source_domain = possible_sources[int(request.form["options"])]
-            print("SOURCE domain: " + str(source_domain))
-            counter = 0
 
-            # RUN MODEL HERE
-            
-
-
-    # default homepage view
+    
+    # if button is pressed
     return render_template(
-        "index_final_tokens.html", 
+        "index.html", 
         options=options,
         show_domains="", 
         dropdown_show="display: none;",
@@ -119,11 +80,6 @@ def show_domains():
         target_selection_show = "display: none;",
         source_domain_show="display: none;",
         possible_sources=[],
-        input_show="",
-        new_sent = "",
-        first_el = "",
-        second_el = "",
-        third_el = "",
     )
 
 
